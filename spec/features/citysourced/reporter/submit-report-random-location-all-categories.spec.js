d91a8d010c7022b69326585200ee7abdffef26e1
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-08-24 11:10:06 -0400

    Fixed bugs and reformatted

--- spec/features/citysourced/reporter/submit-report-random-location-all-categories.spec.js
+++ spec/features/citysourced/reporter/submit-report-random-location-all-categories.spec.js
@@ -1,42 +1,50 @@
-const webdriver = require('selenium-webdriver'),
-	chai = require('chai'),
-	expect = chai.expect,
-	_ = require('lodash'),
-	mocha = require('mocha'),
-	jsonTools = require('../../../../utility/jsonTools'),
-	categoryJson = require('../../../../data/categories.json'),
-    By = webdriver.By,
-	until = webdriver.until,
-	stagingUrl = 'http://staging.baltimorecountymd.gov/baltcogo/reporter';
+const webdriver = require('selenium-webdriver');
+const chai = require('chai');
+const expect = chai.expect;
+const _ = require('lodash');
+const jsonTools = require('../../../../utility/jsonTools');
+const categoryJson = require('../../../../data/categories.json');
+const By = webdriver.By;
+const until = webdriver.until;
+const stagingUrl = 'http://staging.baltimorecountymd.gov/baltcogo/reporter';
 
 const getCategoryLeaves = (myData) => {
 	let list = [];
 
 	_.forEach(myData, (value) => {
-			if (value.types) {
-				list.push(getCategoryLeaves(value.types));
-			} else {
-				list.push(value.id);
-			}
-		});
+		if (value.types) {
+			list.push(getCategoryLeaves(value.types));
+		} else {
+			list.push(value.id);
+		}
+	});
 
 	return list;
-}
+};
 
 describe('CitySourced reporter', () => {
-
 	describe('Can submit a report with a random location', () => {
-		
-		let driver, 
-			categoryLeaves, categoryIndex = 0, categoryPath, flattenedCategories, categoryId,
-			categories1, description, address, map, locationDescription, firstName, lastName, 
-			email, deviceNumber, nextButton;
-		
+		let driver;
+		let categoryLeaves;
+		let categoryIndex = 0;
+		let categoryPath;
+		let flattenedCategories;
+		let categoryId;
+		let categories1;
+		let description;
+		let address;
+		let map;
+		let firstName;
+		let lastName;
+		let email;
+		let deviceNumber;
+		let nextButton;
+
 		categoryLeaves = getCategoryLeaves(categoryJson);
 		flattenedCategories = _.flattenDeep(categoryLeaves);
 
 		before(() => {
-			driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();			
+			driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 		});
 
 		beforeEach(() => {
@@ -54,16 +62,15 @@ describe('CitySourced reporter', () => {
 			nextButton = driver.findElement(By.id('nextButton'));
 			categoryId = flattenedCategories[categoryIndex];
 			categoryPath = _.flattenDeep(jsonTools.getSubtreePath(categoryJson, 'id', 'types', categoryId));
-			categoryIndex++;
+			categoryIndex += 1;
 		});
 
-		after(function() {
+		after(function () {
 			driver.quit();
 		});
 
 		_.forEach(flattenedCategories, (category) => {
 			it('Can submit a report for ' + category, (done) => {
-
 				// First panel ------ category and description
 
 				categories1.findElement(By.css('option[value="' + categoryPath[0] + '"]')).click().then(() => {
@@ -73,14 +80,15 @@ describe('CitySourced reporter', () => {
 					}
 				});
 
+
 				driver.findElement(By.id('pet-type')).then((foundElement) => foundElement.findElement(By.css('option:nth-child(2)')).click(), () => { return true; });
-				
+
 				driver.findElement(By.id('pet-sex')).then((foundElement) => foundElement.findElement(By.css('option:nth-child(2)')).click(), () => { return true; });
-				
+
 				driver.findElement(By.id('primary-color')).then((foundElement) => foundElement.findElement(By.css('option:nth-child(2)')).click(), () => { return true; });
-				
+
 				driver.findElement(By.id('primary-breed')).then((foundElement) => foundElement.findElement(By.css('option:nth-child(2)')).click(), () => { return true; });
-				
+
 				driver.findElement(By.id('animalDescription')).then((foundElement) => foundElement.sendKeys('Animal description text.'), () => {});
 
 				description.sendKeys('This is a sample description. Do not be alarmed by how samply it is.');
@@ -95,58 +103,56 @@ describe('CitySourced reporter', () => {
 
 				let clickTheMap = () => {
 					map.getSize().then((size) => {
-						const clickX = Math.floor(Math.random() * size.width),
-							clickY = Math.floor(Math.random() * size.height);
-						
-						driver.actions()
-							.mouseMove(map, {x : clickX, y: clickY})
-							.click()
-							.perform();
+						if (size.width !== 0) {
+							const clickX = Math.floor(Math.random() * size.width);
+							const clickY = Math.floor(Math.random() * size.height);
+
+							driver.actions()
+								.mouseMove(map, { x: clickX, y: clickY })
+								.click()
+								.perform();
+						}
 					});
 				};
-				
+
 				clickTheMap();
 
 				driver.sleep(1000);
 
-				address.getAttribute('value').then((value) => {
-					if (!value) {
-						clickTheMap();
-						driver.sleep(1000);
-					}
-
-					address.getAttribute('value').then((value) => {
-						if (!value) {
-							clickTheMap();
-							driver.sleep(1000);
-						}
-
-						address.getAttribute('value').then((value) => {
-							if (!value) {
+				address.isDisplayed().then((isDisplayed) => {
+					if (isDisplayed) {
+						address.getAttribute('value').then((value1) => {
+							if (!value1) {
 								clickTheMap();
 								driver.sleep(1000);
 							}
-						});	
 
-					});	
+							address.getAttribute('value').then((value2) => {
+								if (!value2) {
+									clickTheMap();
+									driver.sleep(1000);
+								}
 
-				});	
+								address.getAttribute('value').then((value3) => {
+									if (!value3) {
+										clickTheMap();
+										driver.sleep(1000);
+									}
+								});
+							});
 
-				driver.findElement(By.id('locationDescription')).then((locationDescription) => {
-					if (locationDescription) {
-						locationDescription.sendKeys('This is a description of the location.');
-						nextButton.click();
+							nextButton.click().then(
+								() => driver.sleep(1000),
+								(err) => {
+									console.log(err);
+								});
+						});
 					}
-				}, 
-				() => {
-					nextButton.click();
 				});
-				
-				driver.sleep(1000);
-				
+
 				let fileReportButton = driver.findElement(By.id('fileReportButton'));
 
-				firstName.sendKeys('Selenium');
+				firstName.sendKeys('Test');
 				lastName.sendKeys('Automation');
 
 				// These 4 elements are conditional
@@ -160,7 +166,7 @@ describe('CitySourced reporter', () => {
 
 				fileReportButton.click().then(() => {
 					driver.wait(until.elementLocated(By.css('.bc-citysourced-reporter-alert.alert-success h2')), 10000).then((successMessage) => {
-						successMessage.getText().then((actual) => {						
+						successMessage.getText().then((actual) => {
 							expect(actual).to.equal('Your Submission Has Been Received', 'Success message not visible.');
 							done();
 						});
@@ -168,5 +174,5 @@ describe('CitySourced reporter', () => {
 				});
 			});
 		});
-	});	
+	});
 });
