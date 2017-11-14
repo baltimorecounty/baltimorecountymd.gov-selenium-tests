657db3f8e27545fd69c92b90d532a9de47c7c0ab
Marty Powell <jpowel25@gmail.com>
2017-11-14 10:27:52 -0500

    refactored to use a more es6 style

--- spec/features/homepage/homepage-carousel.automater.js
+++ spec/features/homepage/homepage-carousel.automater.js
@@ -1,113 +1,108 @@
 const webdriver = require('selenium-webdriver');
+
 const By = webdriver.By;
-const until = webdriver.until;
 
-function Automater(driver) {
-    var self = this;
-    self.driver = driver;
 
-    const carouselItemSelector = '.carousel .carousel-item';
-    const carouselActiveItemImageSelector = carouselItemSelector + '.slick-active img';
-    const carouselSelector = '.carousel';
-    const searchControlSelector = '#search-container';
+class Automater {
 
-    self.checkForRotation = function (rotationInterval) {
-        return new Promise((resolve, reject) => {
-            return getActiveImgSrc()
-                .then((src) => {
-                    setTimeout(() => {
-                        return getActiveImgSrc().then((newSrc) => {
-                            const isRotating = src !== newSrc;
-                            resolve(isRotating);
-                        });
-                    }, rotationInterval);
-                })
-                .catch(reject);
-        });
-    };
+	constructor(options) {
+		const handleError = (msg) => {
+			console.error(msg); // eslint-disable-line no-console
+		};
 
-    self.getItems = function () {
-        return new Promise((resolve, reject) => {
-            return driver.findElements(By.css(carouselItemSelector))
-                .then(resolve)
-                .catch(reject);
-        });
-    };
+		this.driver = options.driver || handleError('');
+		this.carouselItemSelector = options.carouselItemSelector || handleError('');
+		this.carouselActiveItemImageSelector = options.carouselActiveItemImageSelector || handleError('');
+		this.carouselSelector = options.carouselSelector || handleError('');
+		this.searchControlSelector = options.searchControlSelector || handleError('');
+	}
 
-    self.isCarouselBehindSearch = function () {
-        return new Promise((resolve, reject) => {
-            return getCssValueBySelector(carouselSelector, 'z-index')
-                .then((carouselZIndex) => {
-                    carouselZIndex = carouselZIndex === 'auto' ? 0 : carouselZIndex;
+	checkForRotation(rotationInterval) {
+		const self = this;
+		return new Promise((resolve, reject) => self.getActiveImgSrc(self)
+			.then((src) => {
+				setTimeout(() => self.getActiveImgSrc(self)
+					.then((newSrc) => {
+						const isRotating = src !== newSrc;
+						resolve(isRotating);
+					}), rotationInterval);
+			})
+			.catch(reject));
+	}
 
-                    return getCssValueBySelector(searchControlSelector, 'z-index')
-                        .then((searchControlZIndex) => {
-                            const isCarouselBehindSearch = carouselZIndex < searchControlZIndex;
-                            resolve(isCarouselBehindSearch);
-                        })
-                        .catch(reject);
-                })
-                .catch(reject);
-        });
-    };
+	// eslint-disable-next-line class-methods-use-this
+	getActiveImgSrc(automater) {
+		return new Promise((resolve, reject) =>
+			automater.driver.findElement(By.css(automater.carouselActiveItemImageSelector))
+				.then(elm => elm.getAttribute('src')
+					.then(resolve)
+					.catch(reject))
+			.catch(reject));
+	}
 
-    self.isCarouselPaused = function (delay) {
-        return new Promise((resolve, reject) => {
-            return driver.findElement(By.css(carouselSelector))
-                .then(hoverOverElm)
-                .then(getActiveImgSrc)
-                .then((src) => {
-                    setTimeout(() => {
-                        return getActiveImgSrc()
-                            .then((newSrc) => {
-                                const isCarouselPaused = src === newSrc;
-                                resolve(isCarouselPaused);
-                            })
-                            .catch(reject);
-                    }, delay);
-                })
-                .catch(reject);
-        });
-    };
+	static getCssValueByElement(elm, prop) {
+		return elm.getCssValue(prop);
+	}
 
-    function getActiveImgSrc() {
-        return new Promise((resolve, reject) => {
-            return driver.findElement(By.css(carouselActiveItemImageSelector))
-                .then((elm) => {
-                    return elm.getAttribute('src')
-                        .then(resolve)
-                        .catch(reject);
-                })
-                .catch(reject)
-        });
-    }
+	getCssValueBySelector(selector, prop) {
+		const self = this;
+		return self.getSingleElement(selector)
+			.then(elm => Automater.getCssValueByElement(elm, prop));
+	}
 
-    /**
-     * Returns a Promise with css value as a string?
-     * @param {*} elm 
-     * @param {*} prop 
-     */
-    function getCssValueByElement(elm, prop) {
-        return elm.getCssValue(prop);
-    }
+	getItems() {
+		const self = this;
+		return new Promise((resolve, reject) =>
+			self.driver.findElements(By.css(self.carouselItemSelector))
+				.then(resolve)
+				.catch(reject));
+	}
 
-    function getCssValueBySelector(selector, prop) {
-        return getSingleElement(selector)
-            .then((elm) => getCssValueByElement(elm, prop));
-    }
+	getSingleElement(selector) {
+		const self = this;
+		return new Promise((resolve, reject) =>
+			self.driver.findElement(By.css(selector))
+				.then(resolve)
+				.catch(reject));
+	}
 
-    function getSingleElement(selector) {
-        return new Promise(function (resolve, reject) {
-            return driver.findElement(By.css(selector))
-                .then(resolve)
-                .catch(reject);
-        });
-    }
+	static hoverOverElm(elm, automater) {
+		return automater.driver.actions().mouseMove(elm).perform();
+	}
 
-    function hoverOverElm(elm) {
-        return driver.actions().mouseMove(elm).perform();
-    }
+	isCarouselBehindSearch() {
+		const self = this;
+		return new Promise((resolve, reject) =>
+			self.getCssValueBySelector(self.carouselSelector, 'z-index')
+				.then((carouselZIndex) => {
+					const carouselZIndexValue = carouselZIndex === 'auto' ? 0 : carouselZIndex;
+					return self.getCssValueBySelector(self.searchControlSelector, 'z-index')
+						.then((searchControlZIndex) => {
+							const isCarouselBehindSearch = carouselZIndexValue < searchControlZIndex;
+							resolve(isCarouselBehindSearch);
+						})
+						.catch(reject);
+				})
+				.catch(reject));
+	}
 
+	isCarouselPaused(delay) {
+		const self = this;
+		return new Promise((resolve, reject) =>
+			self.driver.findElement(By.css(self.carouselSelector))
+				.then(elm => Automater.hoverOverElm(elm, self)
+						.then(() => self.getActiveImgSrc(self)
+								.then((src) => {
+									setTimeout(() =>
+										self.getActiveImgSrc(self)
+											.then((newSrc) => {
+												const isCarouselPaused = src === newSrc;
+												resolve(isCarouselPaused);
+											})
+											.catch(reject), delay);
+								})))
+				.catch(reject));
+	}
 }
 
 module.exports = Automater;
--- spec/features/homepage/homepage-carousel.spec.js
+++ spec/features/homepage/homepage-carousel.spec.js
@@ -1,68 +1,82 @@
+/* eslint-env mocha */
 const webdriver = require('selenium-webdriver');
 const chai = require('chai');
-const expect = chai.expect;
-const _ = require('lodash');
-const By = webdriver.By;
-const until = webdriver.until;
 const constants = require('./constants');
 const Automater = require('./homepage-carousel.automater');
+
+const expect = chai.expect;
+
+const carouselItemSelector = '.carousel .carousel-item';
+const carouselActiveItemImageSelector = `${carouselItemSelector}.slick-active img`;
+const carouselSelector = '.carousel';
+const searchControlSelector = '#search-container';
+
 let driver;
 let automater;
 
 describe('Homepage - Carousel', () => {
-    it('Should contain at least one item', (done) => {
-        automater.getItems()
-            .then((items) => {
-                expect(items.length).to.least(1);
-                done();
-            })
-            .catch((err) => handleException(err, done));
-    });
+	it('Should contain at least one item', (done) => {
+		automater.getItems()
+			.then((items) => {
+				expect(items.length).to.least(1);
+				done();
+			})
+			.catch(err => handleException(err, done));
+	});
 
-    it('Should change slides every 5 seconds if there is more than 1 item', (done) => {
-        automater.checkForRotation(constants.carouselDelay)
-            .then((isRotating) => {
-                expect(isRotating).to.equal(true);
-                done();
-            })
-            .catch((err) => handleException(err, done));
-    });
+	it('Should change slides every 5 seconds if there is more than 1 item', (done) => {
+		automater.checkForRotation(constants.carouselDelay)
+			.then((isRotating) => {
+				expect(isRotating).to.equal(true);
+				done();
+			})
+			.catch(err => handleException(err, done));
+	});
 
-    it('Should be displayed behind the search component', (done) => {
-        automater.isCarouselBehindSearch()
-            .then((isCarouselBehindSearch) => {
-                expect(isCarouselBehindSearch).to.equal(true);
-                done();
-            })
-            .catch((err) => handleException(err, done));
-    });
+	it('Should be displayed behind the search component', (done) => {
+		automater.isCarouselBehindSearch()
+			.then((isCarouselBehindSearch) => {
+				expect(isCarouselBehindSearch).to.equal(true);
+				done();
+			})
+            .catch(err => handleException(err, done));
+	});
 
-    it('Should pause rotation if the mouse is hovered over an item', (done) => {
-        automater.isCarouselPaused(10000)
-            .then((isCarouselPaused) => {
-                expect(isCarouselPaused).to.equal(true);
-                done();
-            })
-            .catch((err) => handleException(err, done));
-    });
+	it('Should pause rotation if the mouse is hovered over an item', (done) => {
+		automater.isCarouselPaused(10000)
+			.then((isCarouselPaused) => {
+				expect(isCarouselPaused).to.equal(true);
+				done();
+			})
+			.catch(err => handleException(err, done));
+	});
 
-    before(() => {
-        driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
-        automater = new Automater(driver);
-    });
+	before(() => {
+		driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
-    beforeEach(() => {
-        driver.get(constants.url);
-    });
+		const testConfig = {
+			driver,
+			carouselItemSelector,
+			carouselActiveItemImageSelector,
+			carouselSelector,
+			searchControlSelector,
+		};
 
-    after(function () {
-        driver.quit();
-    });
+		automater = new Automater(testConfig);
+	});
+
+	beforeEach(() => {
+		driver.get(constants.url);
+	});
+
+	after(() => {
+		driver.quit();
+	});
 });
 
 function handleException(err, done) {
-    console.log(err);
-    let errName = err.name || null;
-    expect(errName).to.equal(null);
-    done();
+	console.log(err); // eslint-disable-line no-console
+	const errName = err.name || null;
+	expect(errName).to.equal(null);
+	done();
 }
