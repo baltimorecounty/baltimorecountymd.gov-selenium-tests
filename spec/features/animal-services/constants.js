c76db26985e37a2194eb3f33764bce7337000a54
Marty Powell <jpowel25@gmail.com>
2017-11-17 14:17:19 -0500

    added tests and automater

---
+++ spec/features/animal-services/constants.js
@@ -0,0 +1,14 @@
+const constants = {
+	urls: {
+		adoptAPet: {
+			name: 'Adopt a Pet',
+			url: 'https://www.baltimorecountymd.gov/Agencies/animalservices/adoption.html',
+		},
+		lostPets: {
+			name: 'Lost Pets',
+			url: 'https://www.baltimorecountymd.gov/Agencies/animalservices/lost.html',
+		},
+	},
+};
+
+module.exports = constants;
