f36740f4a54ba4465e04fd40b854e14edab063a7
Marty Powell <jpowel25@gmail.com>
2017-11-20 14:13:58 -0500

    added options

---
+++ spec/features/daily-docket/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+	puppeteerOptions: {
+		appUrl: 'https://www.baltimorecountymd.gov/Agencies/circuit/dailydocket/index.html',
+		options: {},
+	},
+};
+
+module.exports = constants;
